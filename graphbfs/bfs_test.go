bde925387751b28a650b276b9d00c343d72cbe68
momiji-w <foo@bar.com>
2026-05-24 23:52:09 +0700

    updating graph bfs test case

--- graphbfs/bfs_test.go
+++ graphbfs/bfs_test.go
@@ -1,1 +1,15 @@
 package graphbfs
+
+import (
+	"testing"
+	"slices"
+	"github.com/momiji-w/dsa/utils"
+)
+
+func TestBFS(t *testing.T) {
+        res := BFS(utils.G, 0, 6)
+        expected := []int{0, 2, 3, 6}
+        if !slices.Equal(res, expected) {
+                t.Fatalf("expected %v, got %v", expected, res)
+        }
+}
