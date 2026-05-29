2205797790bd919e11d0b3a687dfbeb25ef6d894
momiji <foo@bar.com>
2026-05-29 16:32:56 +0700

    dfs mistakes lol

--- btdfs/dfs.go
+++ btdfs/dfs.go
@@ -6,4 +6,8 @@ type path struct {
 	path []int
 }
 
+func (p *path) append(v int) {
+	p.path = append(p.path, v)
+}
+
 func DFS(node *utils.BinaryNode) []int
--- btdfs/dfs_test.go
+++ btdfs/dfs_test.go
@@ -6,16 +6,6 @@ import (
 	"testing"
 )
 
-func TestBFS(t *testing.T) {
-	expected := []int{10, 11, 12, 13, 14, 15, 16}
-
-	path := BFS(utils.Root)
-
-	if !slices.Equal(path, expected) {
-		t.Fatalf("%v != %v\n", path, expected)
-	}
-}
-
 func TestDFS(t *testing.T) {
 	expected := []int{13, 11, 14, 10, 15, 12, 16}
 
@@ -23,15 +13,5 @@ func TestDFS(t *testing.T) {
 
 	if !slices.Equal(path, expected) {
 		t.Fatalf("%v != %v\n", path, expected)
-	}
-}
-
-func TestCompare(t *testing.T) {
-	if !Compare(root, root) {
-		t.Fatalf("Tree should be identical")
-	}
-
-	if Compare(root, root2) {
-		t.Fatalf("Tree not should be identical")
 	}
 }
